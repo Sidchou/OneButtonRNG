85a99b63bd00258200be35b8edf7cdf87b2a5795
Sid <sidchou93@gmail.com>
2019-02-06 23:17:36 -0500

    Initial Commit

---
+++ RNG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNG
+//
+//  Created by ching Hsi chou on 2/6/19.
+//  Copyright © 2019 ching Hsi chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
